e548773d2e8e063e3c3fa8ca51feabe8c7a06d92
marynanesterenko <marynanesterenko.qa@gmail.com>
2025-05-16 00:01:03 -0400

    Introducing 'host' variable for more flexibility with the remote test execution in a future

--- src/test/java/com/digitalbank/automation/config/ConfigurationsPOJO.java
+++ src/test/java/com/digitalbank/automation/config/ConfigurationsPOJO.java
@@ -5,6 +5,7 @@ import java.text.MessageFormat;
 public class ConfigurationsPOJO {
 
     public String baseUrl;
+    public String host;
     public String browser;
     public boolean headless;
     public DbConfig dbUrl;
@@ -13,11 +14,19 @@ public class ConfigurationsPOJO {
         public String url;
         public String username;
         public String password;
+
+        @Override
+        public String toString() {
+            return MessageFormat.format(
+                    "DbConfig'{'url=''{0}'', username=''{1}'', password=''{2}''",
+                    url, username, password);
+        }
     }
 
     @Override
     public String toString() {
         return MessageFormat.format(
-                "ConfigurationsPOJO'{'baseUrl=''{0}'', browser=''{1}'', headless={2}, dbUrl={3}'}'", baseUrl, browser, headless, dbUrl);
+                "ConfigurationsPOJO'{'baseUrl=''{0}'', host = ''{1}'', browser=''{2}'', headless={3}, dbUrl={4}'}'",
+                baseUrl, host, browser, headless, dbUrl);
     }
 }
