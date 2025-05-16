25b77abe00deced4aa42b8e7c168fbf508204cce
marynanesterenko <marynanesterenko.qa@gmail.com>
2025-05-15 23:22:10 -0400

    Implemented YAML config loader using Jackson with SLF4J logging

---
+++ src/test/java/com/digitalbank/automation/config/ConfigsReader.java
@@ -0,0 +1,31 @@
+package com.digitalbank.automation.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.InputStream;
+
+public class ConfigsReader {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConfigsReader.class);
+    private static ConfigurationsPOJO config;
+
+    public static ConfigurationsPOJO getConfig() {
+        if (config == null) {
+            LOGGER.info("Loading configuration from config.yaml");
+            try (InputStream input = ConfigsReader.class
+                    .getClassLoader()
+                    .getResourceAsStream("config.yaml")) {
+
+                ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+                config = mapper.readValue(input, ConfigurationsPOJO.class);
+            } catch (Exception e) {
+                LOGGER.error("Error loading configuration file", e);
+                throw new RuntimeException("Failed to load configuration file", e);
+            }
+        }
+        return config;
+    }
+}
